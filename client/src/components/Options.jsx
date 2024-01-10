e8574d21805370264d5f5214ec658022ffd61248
gingerninja412 <samdpeevor@gmail.com>
2024-01-10 15:22:51 +0000

    options mock up board

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,7 +1,11 @@
+import { CgProfile } from "react-icons/cg";
+import { GrMapLocation } from "react-icons/gr";
+
 function Options() {
   return ( 
     <div id="options">
-
+      <CgProfile size="60"/>
+      <GrMapLocation size="60"/>
     </div>
    );
 }
