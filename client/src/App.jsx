43175f4c69d8afdd420ae63606b36fcf414c81d2
gingerninja412 <samdpeevor@gmail.com>
2024-01-10 14:36:11 +0000

    added the route to the map screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import MapScreen from './components/MapScreen'
+import Options from './components/Options'
 
 function App() {
 
   return (
-    <div id='app'></div>
+    <div id='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/mapScreen" element={<div><MapScreen /><Options /></div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
