38e0523fb5590e520f6d9ba7ccbc68a72582ccdb
gingerninja412 <samdpeevor@gmail.com>
2024-01-10 15:57:17 +0000

    added a global position store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,20 @@ import MapScreen from './components/MapScreen'
 import Options from './components/Options'
 
 function App() {
+  const [newSpotPosition, setNewSpotPosition] = useState(null)
 
   return (
     <div id='app'>
       <BrowserRouter>
         <Routes>
-          <Route path="/mapScreen" element={<div className='dashboard'><MapScreen /><Options /></div>} />
+          <Route path="/mapScreen" element={
+            <div className='dashboard'>
+              <MapScreen 
+                newSpotPosition={newSpotPosition} 
+                setNewSpotPosition={setNewSpotPosition}
+              />
+              <Options />
+            </div>} />
         </Routes>
       </BrowserRouter>
     </div>
