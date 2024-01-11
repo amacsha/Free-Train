34828386778b678414ce77cce15a6a03e2b8d85c
gingerninja412 <samdpeevor@gmail.com>
2024-01-11 17:49:08 +0000

    styled the expanded spot component

--- client/src/components/spotExpanded.jsx
+++ client/src/components/spotExpanded.jsx
@@ -11,7 +11,7 @@ function SpotExpanded() {
     axios.get(`http://localhost:3000/spot/getSpot/${params.spotName}`).then(res => {
       console.log(res.data)
       setSpot(res.data)
-      setImagePaths([...imagePaths, res.data.imagePaths])
+      setImagePaths(res.data.imagePaths)
     }).catch(error => {
       console.log(error)
     }) 
@@ -19,15 +19,20 @@ function SpotExpanded() {
 
   return ( 
     <div id="spot-expanded">
-      <Link to="/mapScreen">
-        <button className="back-button"><IoArrowBackCircleOutline size="40"/></button>
-      </Link>
-      <div className="spot-expanded-body">
-        <h1>{parkourSpot.name}</h1>
-        <p>{parkourSpot.description}</p>
-        {imagePaths.map(image => {
-          return <img key={image} src={`http://localhost:3000/spot/getImage/${parkourSpot.name}/${image}`} />
-        })}
+      <div id="spot-expanded-body">
+        <Link to="/mapScreen">
+          <button className="back-button"><IoArrowBackCircleOutline size="40"/></button>
+        </Link>
+        <h1 className="expanded-item">{parkourSpot.name}</h1>
+        <h2 className="expanded-item">Found by {parkourSpot.author}</h2>
+        <div className="divider"></div>
+        <div className="image-show">
+          {imagePaths.map(image => {
+            return <img key={image} src={`http://localhost:3000/spot/getImage/${parkourSpot.name}/${image}`} width="200px"/>
+          })}
+        </div>
+        <div className="divider expanded-item"></div>
+        <p className="description expanded-item">{parkourSpot.description}</p>
       </div>
     </div>
   );
