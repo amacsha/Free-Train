0542a84fc6625a685c7eb323ea4e0206941d9c88
gingerninja412 <samdpeevor@gmail.com>
2024-01-12 10:17:41 +0000

    styled profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -12,26 +12,31 @@ function Profile(props) {
       console.log(error)
     })
   }, [])
-  return ( 
+  return (
     <div id="profile">
-      <Link to="/mapScreen">
-        <button className="back-button"><IoArrowBackCircleOutline size="40"/></button>
-      </Link>
-      <div id="your-spots">
-        <h1>Your Spots</h1>
-        {spots.map(spot => {
-          console.log(spot)
-          return (
-            <div key={spot.name}>
-              <div>
-                <h1>{spot.name}</h1>
+      <div id="profile-body">
+        <Link to="/mapScreen">
+          <button className="back-button"><IoArrowBackCircleOutline size="40"/></button>
+        </Link>
+        <h1 className="profile-item">{props.user}</h1>
+        <h2 className="profile-header">Your Spots</h2>
+        <div className="your-spots">
+          {spots.map(spot => {
+            console.log(spot)
+            return (
+              <div key={spot.name} className="profile-spot">
+                <div className="profile-spot-info">
+                  <h3>{spot.name}</h3>
+                  <button>more info</button>
+                </div>
+                <div>
+                  <img src={`http://localhost:3000/spot/getImage/${spot.name}/${spot.imagePaths[0]}`} height="100px"/>
+                </div>
               </div>
-              <div>
-                <img src={`http://localhost:3000/spot/getImage/${spot.name}/${spot.imagePaths[0]}`}/>
-              </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
+        <div className="field-divider"></div>
       </div>
     </div>
   );
